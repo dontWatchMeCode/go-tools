8a7b4b7fcd803b6fe56df1678c016b40f7f7c0df
M. Gschwandtner <84477901+dontWatchMeCode@users.noreply.github.com>
2023-10-26 16:03:42 +0200

    split crawler into function, added mutex

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"net/url"
 	"strings"
+	"sync"
 
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/queue"
 	"github.com/pterm/pterm"
+)
+
+var (
+	urlChannel   = make(chan urlWithStatus)
+	requestCount = 0
+	errorCount   = 0
+	isDone       = false
 )
 
 type urlWithStatus struct {
@@ -16,24 +24,36 @@ type urlWithStatus struct {
 }
 
 func Start() {
+	initialUrl := getInputURL()
+	if initialUrl == "" {
+		return
+	}
+
+	go runCrawler(initialUrl)
+	displayResults()
+}
+
+func getInputURL() string {
 	result, _ := pterm.DefaultInteractiveTextInput.Show("URL")
 
 	_, err := url.ParseRequestURI(result)
 	if err != nil {
 		pterm.Error.Println("Error parsing URL:", err)
-		return
+		return ""
 	}
 
-	initialUrl := result
+	return result
+}
 
-	urlChannel := make(chan urlWithStatus)
-	requestCount := 0
-	errorCount := 0
-
+func runCrawler(initialUrl string) {
 	c := colly.NewCollector()
 	q, _ := queue.New(10, &queue.InMemoryQueueStorage{MaxSize: 10000})
+	var mu sync.Mutex
 
 	c.OnScraped(func(r *colly.Response) {
+		mu.Lock()
+		defer mu.Unlock()
+
 		requestCount++
 		urlChannel <- urlWithStatus{
 			url:    r.Request.URL.String(),
@@ -42,40 +62,40 @@ func Start() {
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
+		mu.Lock()
+		defer mu.Unlock()
+
 		errorCount++
 		urlChannel <- urlWithStatus{
 			url:    r.Request.URL.String(),
 			status: r.StatusCode,
 		}
-
 	})
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		url := e.Request.AbsoluteURL(e.Attr("href"))
+		mu.Lock()
+		defer mu.Unlock()
 
+		url := e.Request.AbsoluteURL(e.Attr("href"))
 		if url == "" {
 			return
 		}
-
 		if strings.HasPrefix(url, initialUrl) {
 			q.AddURL(url)
 		}
 	})
 
-	isDone := false
-
 	go func() {
 		q.AddURL(initialUrl)
 		q.Run(c)
 		c.Wait()
-
 		isDone = true
-		fmt.Println(isDone)
 	}()
+}
 
+func displayResults() {
 	limit := 5
 	data := []urlWithStatus{}
-
 	area, _ := pterm.DefaultArea.Start()
 
 	for {
@@ -83,14 +103,10 @@ func Start() {
 
 		msg, ok := <-urlChannel
 		if ok {
-
 			if len(data) >= limit {
 				data = data[1:]
 			}
-			data = append(data, urlWithStatus{
-				url:    msg.url,
-				status: msg.status,
-			})
+			data = append(data, msg)
 		} else {
 			break
 		}
@@ -101,7 +117,6 @@ func Start() {
 
 		str += fmt.Sprintf("\n-> requests: %d", requestCount)
 		str += fmt.Sprintf("\n-> errors: %d", errorCount)
-
 		str += "\n"
 
 		area.Update(str)
