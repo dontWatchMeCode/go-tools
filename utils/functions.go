f031c9af3b49d202774b5061ea943560c413eb38
M. Gschwandtner <84477901+dontWatchMeCode@users.noreply.github.com>
2023-10-27 04:16:34 +0200

    changed exit added CreateTempFile

--- utils/functions.go
+++ utils/functions.go
@@ -1,12 +1,26 @@
 package utils
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/pterm/pterm"
 )
 
 func Exit() {
-	pterm.Warning.Println("Exiting...")
+	pterm.Warning.Println("Exiting ...")
 	os.Exit(0)
+}
+
+func CreateTempFile(identifier string) (*os.File, error) {
+	fileName := fmt.Sprintf("dontWatchMeCode-go-tools-%s-%d.csv", identifier, time.Now().Unix())
+	tempFilePath := filepath.Join("/tmp", fileName)
+
+	file, err := os.OpenFile(tempFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
 }
