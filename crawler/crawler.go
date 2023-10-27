686265223492dffa085122a30d6493c33e3f688c
M. Gschwandtner <84477901+dontWatchMeCode@users.noreply.github.com>
2023-10-27 04:17:15 +0200

    crawler changes / cleanup

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -6,8 +6,10 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/dontWatchMeCode/go-tools/utils"
@@ -22,33 +24,29 @@ var done = false
 var statusMap = cmap.New[int]()
 var sourceMap = cmap.New[string]()
 
-var logFile *os.File
+var logFiles = make(map[string]*os.File)
+var logFileMu sync.Mutex
+var logPrefix = ""
 
 func Start() {
-	if file, err := createTempLogFile(); err == nil {
-		logFile = file
-		defer logFile.Close()
-		defer os.Remove(logFile.Name())
-	}
-
 	initialUrl := getInputURL()
+	logPrefix = getFileNamePrefix(initialUrl)
+
 	if initialUrl == "" {
 		return
 	}
 
 	go renderInfoDisplay(initialUrl)
 	runCrawler(initialUrl)
-}
+	writeToFiles(initialUrl)
 
-func createTempLogFile() (*os.File, error) {
-	fileName := fmt.Sprintf("dontWatchMeCode-go-tools-crawl-%d.csv", time.Now().Unix())
-	tempFilePath := filepath.Join("/tmp", fileName)
+	for _, file := range logFiles {
+		if strings.Contains(file.Name(), "-temp") {
+			os.Remove(file.Name())
+		}
 
-	file, err := os.OpenFile(tempFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
-	if err != nil {
-		return nil, err
+		file.Close()
 	}
-	return file, nil
 }
 
 func getInputURL() string {
@@ -57,58 +55,76 @@ func getInputURL() string {
 	_, err := url.ParseRequestURI(result)
 	if err != nil {
 		pterm.Error.Println("Error parsing URL:", err)
+		utils.Exit()
 		return ""
 	}
 
 	return result
 }
 
+func getFileNamePrefix(initialUrl string) string {
+	result, _ := pterm.DefaultInteractiveTextInput.WithOnInterruptFunc(utils.Exit).Show("File prefix (leave blank for url + timestamp)")
+
+	if result == "" {
+		result = utils.RemoveHttpPrefix(initialUrl) + "_" + fmt.Sprintf("%d", time.Now().Unix())
+		result = strings.ReplaceAll(result, "www.", "")
+		result = strings.ReplaceAll(result, ".", "_")
+	}
+
+	specialCharPattern := regexp.MustCompile("[^a-zA-Z0-9_]+")
+	return specialCharPattern.ReplaceAllString(result, "")
+}
+
 func runCrawler(initialUrl string) {
 	transport := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
 	httpClient := &http.Client{
 		Transport: transport,
-		Timeout:   30 * time.Second,
+		Timeout:   300 * time.Second,
 	}
 
 	c := colly.NewCollector(colly.AllowedDomains(utils.RemoveHttpPrefix(initialUrl)))
 	c.SetClient(httpClient)
 
-	q, _ := queue.New(100, &queue.InMemoryQueueStorage{})
+	q, _ := queue.New(50, &queue.InMemoryQueueStorage{})
 
 	c.OnScraped(func(r *colly.Response) {
-		logUrl(r.Request.URL.String(), r.StatusCode)
-		statusMap.Set(r.Request.URL.String(), r.StatusCode)
+		processUrl(httpClient, r.Request.URL.String(), r.StatusCode)
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
-		logUrl(r.Request.URL.String(), r.StatusCode)
-		statusMap.Set(r.Request.URL.String(), r.StatusCode)
+		processUrl(httpClient, r.Request.URL.String(), r.StatusCode)
 	})
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		url := e.Request.AbsoluteURL(e.Attr("href"))
-		url = strings.TrimRight(url, "/")
+		urlVal := e.Request.AbsoluteURL(e.Attr("href"))
 
-		if url != "" {
-			sourceMap.Set(url, e.Request.URL.String())
-			q.AddURL(url)
+		urlVal = strings.TrimRight(urlVal, "/")
+		urlVal = strings.TrimSpace(urlVal)
+
+		if urlVal != "" {
+			sourceMap.Set(urlVal, e.Request.URL.String())
+			q.AddURL(urlVal)
 		}
 
 		go func() {
-			if !strings.HasPrefix(url, initialUrl) {
-				if statusMap.Has(url) {
+			if !strings.HasPrefix(urlVal, initialUrl) {
+				if statusMap.Has(urlVal) {
 					return
 				}
 
-				statusMap.Set(url, -1)
+				statusMap.Set(urlVal, 0)
 
-				getRequest, err := httpClient.Get(url)
+				_, err := url.ParseRequestURI(urlVal)
 				if err != nil {
 					return
 				}
 
-				logUrl(url, getRequest.StatusCode)
-				statusMap.Set(url, getRequest.StatusCode)
+				getRequest, err := httpClient.Get(urlVal)
+				if err != nil {
+					return
+				}
+
+				processUrl(httpClient, urlVal, getRequest.StatusCode)
 			}
 		}()
 	})
@@ -120,13 +136,74 @@ func runCrawler(initialUrl string) {
 	done = true
 }
 
+func writeToFiles(initialUrl string) {
+	for url, status := range statusMap.Items() {
+		firstStatusNumber := string(strconv.Itoa(status)[0])
+
+		source, ok := sourceMap.Get(url)
+		if !ok {
+			source = ""
+		}
+
+		if url == "" && status == 0 && source == "" {
+			continue
+		}
+
+		logStringToFileCreateIfNotExists(
+			logPrefix+"-full.csv",
+			fmt.Sprintf("%d|%s|%s|\n", status, url, source),
+		)
+
+		logStringToFileCreateIfNotExists(
+			logPrefix+"-"+firstStatusNumber+"xx.csv",
+			fmt.Sprintf("%d|%s|%s|\n", status, url, source),
+		)
+
+		if !strings.HasPrefix(url, initialUrl) {
+			logStringToFileCreateIfNotExists(
+				logPrefix+"-external.csv",
+				fmt.Sprintf("%d|%s|%s|\n", status, url, source),
+			)
+		}
+	}
+}
+
+func processUrl(httpClient *http.Client, url string, status int) {
+	if status == 0 {
+		r, err := httpClient.Head(url)
+		if err != nil {
+			panic(r)
+		}
+		status = r.StatusCode
+	}
+
+	logUrl(url, status)
+	statusMap.Set(url, status)
+}
+
 func logUrl(url string, status int) {
 	source, ok := sourceMap.Get(url)
 	if !ok {
 		source = ""
 	}
 
-	logFile.WriteString(fmt.Sprintf("%d|%s|%s\n", status, url, source))
+	logStringToFileCreateIfNotExists(
+		logPrefix+"-temp.csv",
+		fmt.Sprintf("%d|%s|%s|\n", status, url, source),
+	)
+}
+
+func logStringToFileCreateIfNotExists(fileName string, str string) {
+	logFileMu.Lock()
+	defer logFileMu.Unlock()
+
+	if logFiles[fileName] == nil {
+		if file, err := os.OpenFile(fileName, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644); err == nil {
+			logFiles[fileName] = file
+		}
+	}
+
+	logFiles[fileName].WriteString(str)
 }
 
 func renderInfoDisplay(initialUrl string) {
@@ -134,19 +211,8 @@ func renderInfoDisplay(initialUrl string) {
 
 	for {
 		str := fmt.Sprintf("crawling: %s\n", initialUrl)
-		str += fmt.Sprintf("logfile: %s\n", logFile.Name())
 
-		count := 0
-		errors := 0
-
-		for status := range statusMap.Keys() {
-			count++
-			if status >= 200 && status <= 399 {
-				errors++
-			}
-		}
-
-		str += pterm.DefaultBox.Sprintf("error: %d\ncrawled: %d", errors, count)
+		str += pterm.DefaultBox.Sprintf("crawled: %d", statusMap.Count())
 
 		str += "\n"
 
